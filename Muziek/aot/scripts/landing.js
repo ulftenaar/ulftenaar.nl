f9084ee928b8824b7d55c73e459b757030898735
Robin <robin@Mac-mini-van-Robin.local>
2026-02-24 01:45:55 +0100

    landingpage geupadte, met verdwijning

---
+++ Muziek/aot/scripts/landing.js
@@ -0,0 +1,27 @@
+land_description = document.querySelector('#land_description')
+land_titel = document.querySelector('#land_titel')
+c_land= document.querySelector('#c_land')
+var state = true
+
+c_land.addEventListener('click', () => {
+	state = !state;
+	check();
+	console.log('yeah')
+
+})
+
+
+function check () {
+if (state==true) {
+	// land_titel.style.visibility='hidden';
+	// land_description.style.visibility='visible'
+	
+	land_titel.style.display='none';
+	land_description.style.display='flex'
+}
+
+else if (state==false) {
+	land_titel.style.display='flex';
+	land_description.style.display='none'
+}
+}
