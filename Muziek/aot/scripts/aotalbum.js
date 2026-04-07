f739f88b60be83c86d53a64ea08dc75aa01541d9
ulftenaar <info.pak_ii@proton.me>
2026-04-07 16:27:10 +0200

    albumsite7april

--- Muziek/aot/scripts/aotalbum.js
+++ Muziek/aot/scripts/aotalbum.js
@@ -1,4 +1,6 @@
 const songtitel = document.querySelector('#songtitel');
+let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 const albumaudio = document.querySelector('#albumaudio')
 let songs = ['duszegdannietdatjenogvanmehoudt', 'kast','24nov25', 'slaapzacht', 'landen', 'hoehetwasomvanjetehouden','blauw','alswesamenbeterzijn','vaarwel','visophetdroge']
 const knop_prev = document.querySelector('#knop_prev');
@@ -9,9 +11,11 @@ const knop_startalbum = document.querySelector('#knop_startalbum')
 const info_nummer = document.querySelector('#info_nummer');
 const playermenu = document.querySelector('.playermenu');
 const sections = document.querySelectorAll('[id^="sec"]')
+const randommedias = document.querySelectorAll('.randommedia')
 
 let songindex = 0;
 let albumplaying = false;
+let blendmodes=["difference", "soft-light", "exclusion", "luminosity", "color-dodge"]
 
 sections[0].scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 playalbum();
@@ -90,11 +94,19 @@ albumaudio.addEventListener('ended', function() {
 }  )
 }
 
+  randommedias.forEach(function (val, i, list) {
+  randommedias[i].style.top=`${(Math.random() *(vh*0.75))}px`;
+  randommedias[i].style.left=`${(Math.random() *(vw*0.75))}px`;
+  randommedias[i].style.width=`${((Math.random() *(vw*0.75)) + vw*0.1)}px`;
+  randommedias[i].style.height=`${((Math.random() *(vh*0.75)) + vh*0.1)}px`;
+  randommedias[i].style.mix_blend_mode=`${blendmodes[Math.floor(Math.random()*blendmodes.length)]}`;
+})
+
 // buttons 
 
-knop_prev.addEventListener('click', prevsong);
-knop_stop.addEventListener('click', stopalbum);
-knop_pause.addEventListener('click', pausealbum);
-knop_next.addEventListener('click', nextsong);
-knop_startalbum.addEventListener('click', playalbum);
+knop_prev.addeventListener('click', prevsong);
+knop_stop.addevenListener('click', stopalbum);
+knop_pause.adeEventListener('click', pausealbum);
+knop_next.addeventListener('click', nextsong);
+knop_startalbum.addeventListener('click', playalbum);
 
